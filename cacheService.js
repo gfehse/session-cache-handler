aa090a5979ea884bb8d993f2ed8bffd59d775d85
gfehse <gfehse@gmail.com>
2015-09-21 23:24:38 -0300

    Updating cache server to retrieve cache data

--- cacheService.js
+++ cacheService.js
@@ -1,16 +1,28 @@
 var http = require('http');
+var _url_ = require('url');
 var cacheAPI = require('./cacheAPI.js');
 
 // Create the http Server Cache
-var cacheServer = http.createServer(requestListener);
-
-// handling http enquires 
-function requestListener(request, response) {
-
-	//TODO: just for a while returning 200 
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('okay');	
-}
-
+var cacheServer = http.createServer(handleRequest);
 cacheServer.listen(15000);
 
+
+function handleRequest(request, response) {
+
+	var method = request.method;
+	var url = _url_.parse(request.url, true);
+	var result = '';
+	if(method=='GET' && url.query && url.query.sessionId) {
+		result = cacheAPI.readCache(url.query.sessionId);
+	}
+
+	if(method=='POST') {
+
+	}
+
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	response.write(result);
+	response.end();
+
+
+}
